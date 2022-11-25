7d59b048b4e5a41a6a3fb41a9fd3cc918203054f
jiayu_yao <jiayu_yao@nuaa.edu.cn>
2022-11-26 04:03:17 +0800

    fix bugs

--- submitit_pretrain.py
+++ submitit_pretrain.py
@@ -32,9 +32,9 @@ def parse_args():
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
-        p.mkdir(exist_ok=True)
+    if Path("./checkpoint/").is_dir():
+        p = Path(f"./checkpoint/{user}/experiments")
+        p.mkdir(parents=True, exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
 
